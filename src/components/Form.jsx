64e9c2f7541181e2048e3840636b4391ce5bf48c
tom-penny <contact@tompenny.com>
2023-02-08 20:42:07 +0000

    create basic form structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,28 @@ import '../styles/form.scss'
 
 export const Form = () => {
 
-    return <div className='form'>
-        
-    </div>
+    return <form className='form'>
+        <input className='form__input' type='text' name='name' placeholder="Event name" required/>
+        <div className='form__menu'>
+            <select className='form__menu__select' name='date'>
+                <option value='Monday'>Monday</option>
+                <option value='Tuesday'>Tuesday</option>
+                <option value='Wednesday'>Wednesday</option>
+                <option value='Thursday'>Thursday</option>
+                <option value='Friday'>Friday</option>
+            </select>
+            <select className='form__menu__select' name='time'>
+                <option value='09:00'>09:00</option>
+                <option value='10:00'>10:00</option>
+                <option value='11:00'>11:00</option>
+                <option value='12:00'>12:00</option>
+                <option value='13:00'>13:00</option>
+                <option value='14:00'>14:00</option>
+                <option value='15:00'>15:00</option>
+                <option value='16:00'>16:00</option>
+                <option value='17:00'>17:00</option>
+            </select>
+            <button className='form__menu__btn' type='submit'>Save</button>
+        </div>
+    </form>
 }
