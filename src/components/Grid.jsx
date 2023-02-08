e01fead05d70084a67e6308cba495ac48391ba0c
tom-penny <contact@tompenny.com>
2023-02-08 22:07:48 +0000

    add grid styling

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -16,20 +16,20 @@ export const Grid = () => {
     return <div className='grid'>
         {createCells()}
 
-        <div className='grid__cell'></div>
-        <div className='grid__cell--col-header'>MON</div>
-        <div className='grid__cell--col-header'>TUE</div>
-        <div className='grid__cell--col-header'>WED</div>
-        <div className='grid__cell--col-header'>THU</div>
-        <div className='grid__cell--col-header'>FRI</div>
-        <div className='grid__cell--row-header'>09:00</div>
-        <div className='grid__cell--row-header'>10:00</div>
-        <div className='grid__cell--row-header'>11:00</div>
-        <div className='grid__cell--row-header'>12:00</div>
-        <div className='grid__cell--row-header'>13:00</div>
-        <div className='grid__cell--row-header'>14:00</div>
-        <div className='grid__cell--row-header'>15:00</div>
-        <div className='grid__cell--row-header'>16:00</div>
-        <div className='grid__cell--row-header'>17:00</div>
+        <div className='grid__cell' style={{gridColumn: "1", gridRow: "1"}}></div>
+        <div className='grid__col-header' style={{gridColumn: "2", gridRow: "1"}}>MON</div>
+        <div className='grid__col-header' style={{gridColumn: "3", gridRow: "1"}}>TUE</div>
+        <div className='grid__col-header' style={{gridColumn: "4", gridRow: "1"}}>WED</div>
+        <div className='grid__col-header' style={{gridColumn: "5", gridRow: "1"}}>THU</div>
+        <div className='grid__col-header' style={{gridColumn: "6", gridRow: "1"}}>FRI</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "2"}}>09:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "3"}}>10:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "4"}}>11:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "5"}}>12:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "6"}}>13:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "7"}}>14:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "8"}}>15:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "9"}}>16:00</div>
+        <div className='grid__row-header' style={{gridColumn: "1", gridRow: "10"}}>17:00</div>
     </div>
 }
