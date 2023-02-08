16e1ca4356ad8882b35a63400223800580cd934b
tom-penny <contact@tompenny.com>
2023-02-08 22:38:39 +0000

    add styling to event elements

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,12 +1,16 @@
 import '../styles/event.scss'
 
 import { useRef } from 'react'
+import { RxCross2 as CrossIcon } from 'react-icons/rx'
 
 const colors = ["#FF6961", "#FFB480", "#F8F38D", "#42D6A4", "#08CAD1", "#59ADF6", "#9D94FF", "#C780E8"]
 
 export const Event = () => {
 
     let width = 100;
+
+    const handleClick = () => {
+    }
 
     // Randomise BG colour and persist through state changes with useRef()
     const color = useRef(colors[Math.floor(Math.random()*colors.length)])
@@ -17,8 +21,7 @@ export const Event = () => {
     }
 
     return <div className='event' style={style}>
-        <div className='event__container'>
-            <div className='event__text'>Name</div>
-        </div>
+        <CrossIcon className='event__icon' onClick={handleClick}/>
+        <div className='event__text'>Name</div>
     </div>
 }
